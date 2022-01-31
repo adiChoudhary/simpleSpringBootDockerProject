c236b31be877137fe45611485b5f7bf420b5721b
Aditya Choudhary <adichoudhary5739@gmail.com>
2022-01-31 21:55:24 +0530

    First Commit

---
+++ src/main/java/com/example/demoDockerProject/simpleDockerProject/SimpleDockerProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoDockerProject.simpleDockerProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDockerProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDockerProjectApplication.class, args);
+	}
+
+}
